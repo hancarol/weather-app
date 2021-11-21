44bb075d2d409f627b4157cfd5adbdfd444cad9f
Caroline Ko <90160199+hancarol@users.noreply.github.com>
2021-11-21 00:01:25 -0500

    added functionality to the Fahrenheit and Celsius link
    added conversion to Fahrenheit and ability to switch css when Celsius and Fahrenheit links are clicked

--- script.js
+++ script.js
@@ -26,12 +26,15 @@ function time() {
 }
 
 function showWeather(response) {
-  let temperature = Math.round(response.data.main.temp);
+  celsiusTemperature = Math.round(response.data.main.temp);
   let iconElement = document.querySelector("#main-icon");
   let windSpeed = response.data.wind.speed;
   let windSpeedMetric = (windSpeed * 3600) / 1000;
+
   document.querySelector("#location").innerHTML = response.data.name;
-  document.querySelector("#main-temperature").innerHTML = `${temperature}`;
+  document.querySelector(
+    "#main-temperature"
+  ).innerHTML = `${celsiusTemperature}`;
   document.querySelector("#wind-speed").innerHTML = Math.round(windSpeedMetric);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#weather-description").innerHTML =
@@ -69,26 +72,37 @@ function getCurrentLocation() {
   navigator.geolocation.getCurrentPosition(showCurrentLocation);
 }
 
-// function changeCelsius() {
-//   let temperature = document.querySelector("#main-temperature");
-//   temperature.innerHTML = "21℃";
-// }
+function displayFahrenheit(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
 
-// function changeFahrenheit() {
-//   let mainTemperature = document.querySelector("#main-temperature");
-//   mainTemperature.innerHTML = response.data.main.temp;
-// }
+  let displayFahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  document.querySelector("#main-temperature").innerHTML = Math.round(
+    displayFahrenheitTemperature
+  );
+}
 
-// let celsius = document.querySelector("#celsius");
-// celsius.addEventListener("click", changeCelsius);
+function displayCelsius(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  document.querySelector("#main-temperature").innerHTML =
+    Math.round(celsiusTemperature);
+}
 
-// let fahrenheit = document.querySelector("#fahrenheit");
-// fahrenheit.addEventListener("click", changeFahrenheit);
+let celsiusTemperature = null;
 
-let search = document.querySelector("#search-location");
-search.addEventListener("submit", submitCity);
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayCelsius);
 
-let searchCurrentLocation = document.querySelector("#current-location");
-searchCurrentLocation.addEventListener("click", getCurrentLocation);
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", displayFahrenheit);
+
+let searchCity = document.querySelector("#search-location");
+searchCity.addEventListener("submit", submitCity);
+
+let currentLocation = document.querySelector("#current-location");
+currentLocation.addEventListener("click", getCurrentLocation);
 
 showCity("new york");
